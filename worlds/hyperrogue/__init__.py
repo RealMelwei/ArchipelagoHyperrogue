ac5b8da049329211851d30908ed0b8b574c82d5b
Melvin Weiß <melvin@weissprivat.de>
2024-07-11 17:36:42 +0200

    Important fix in case of multiple Hyperrogue instances

--- worlds/hyperrogue/__init__.py
+++ worlds/hyperrogue/__init__.py
@@ -19,6 +19,10 @@ location_suffixes: Dict[str,int] = {
         "50 Treasures": 0x300
 }
 
+
+class HyperrogueItem(Item):
+    game="Hyperrogue"
+
 class HyperrogueWorld(World):
     """A tactical puzzle/roguelike on a hyperbolic plane."""
 
@@ -27,31 +31,36 @@ class HyperrogueWorld(World):
 
     game = "Hyperrogue"
 
-    item_name_to_id = {**{land_name : land_id + hyperrogue_base_id
-                       for land_name, land_id in landtable.items()},
-                       **{"Crossroads" : hyperrogue_base_id - 1}}
-    
     # Assigns to each land the number of already created items for that land:
     # This is relevant, as (except for R'Lyeh + Temple of Cthulhu) exactly the first check
     # in each land is relevant for progression.
-    item_creation_progress: Dict[str,int] = {land_name : 0
-                                             for land_name in landlist}
+    item_creation_progress: Dict[str,int] = {}
     
+    item_name_to_id = {**{land_name : land_id + hyperrogue_base_id
+                   for land_name, land_id in landtable.items()},
+                   **{"Crossroads" : hyperrogue_base_id - 1}}
+
     location_name_to_id = {f"{land_name} {suff}" : land_id + hyperrogue_base_id + suff_id
                                     for land_name, land_id in landtable.items()
                                     for suff, suff_id in location_suffixes.items()}
     
+    active_location_suffixes = {}
+
     web = HyperrogueWebWorld()
 
     def generate_early(self) -> None:
+        self.item_creation_progress = {land_name : 0
+                                  for land_name in landlist}
+        self.active_location_suffixes = deepcopy(location_suffixes)
         # Exclude high treasure locations from the game depending on settings
-        if self.options.treasure_requirements<50:
-            del location_suffixes["50 Treasures"]
-        if self.options.treasure_requirements<25:
-            del location_suffixes["25 Treasures"]
+        if self.options.treasure_requirements.value<50:
+            del self.active_location_suffixes["50 Treasures"]
+        if self.options.treasure_requirements.value<25:
+            del self.active_location_suffixes["25 Treasures"]
         pass
 
     def create_regions(self) -> None:
+        print(self.active_location_suffixes)
         # Create regions and add them to the multiworld
         self.multiworld.regions.extend(Region(region_name, self.player, self.multiworld) 
                                        for region_name in regionlist)
@@ -61,7 +70,7 @@ class HyperrogueWorld(World):
             land: Region =  self.multiworld.get_region(land_name,self.player)
             land.add_locations({
                 f"{land_name} {suff}" : self.location_name_to_id[f"{land_name} {suff}"]
-                for suff in location_suffixes.keys()
+                for suff in self.active_location_suffixes.keys()
                 if not suff == "Unlock" # "Unlock" locations belong to the menu region instead
             })
         
@@ -99,22 +108,22 @@ class HyperrogueWorld(World):
         
 
     
-    def create_item(self, name: str) -> Item:
+    def create_item(self, name: str) -> HyperrogueItem:
         # R'Lyeh 10 Treasures and Temple of Cthulhu 10 Treasures are progression relevant,
         # as they allow R'Lyeh to spawn on land
         relevant_progress = 2 if name in ["R'Lyeh", "Temple of Cthulhu"] else 1
         self.item_creation_progress[name] += 1
         if self.item_creation_progress[name] <= relevant_progress:
-            return Item(name, ItemClassification.progression, self.item_name_to_id[name], self.player)
+            return HyperrogueItem(name, ItemClassification.progression, self.item_name_to_id[name], player=self.player)
         else:
-            return Item(name, ItemClassification.useful, self.item_name_to_id[name], self.player)
+            return HyperrogueItem(name, ItemClassification.useful, self.item_name_to_id[name], player=self.player)
 
     def create_items(self) -> None:
         self.multiworld.itempool += [self.create_item(land_name)
                                      for land_name in landlist
-                                     for suff in location_suffixes.keys()]
+                                     for suff in self.active_location_suffixes.keys()]
         # Add Crossroads item and lock it into Crossroads location for initialization purposes
-        cr : Item = Item("Crossroads", ItemClassification.progression_skip_balancing, hyperrogue_base_id - 1, self.player)
+        cr : HyperrogueItem = HyperrogueItem("Crossroads", ItemClassification.progression_skip_balancing, hyperrogue_base_id - 1, player=self.player)
         self.get_location("Crossroads").place_locked_item(cr)
         self.multiworld.push_precollected(cr)
         
@@ -128,11 +137,14 @@ class HyperrogueWorld(World):
         # Add Goal Rule
         self.multiworld.completion_condition[self.player] = get_completion_rule(self.player)
 
+        print(self.multiworld.completion_condition)
+
     def fill_slot_data(self):
         return {
             "death_link": self.options.death_link.value,
             "goal": self.options.goal.value,
-            "starting_land": self.options.starting_land.value
+            "starting_land": self.options.starting_land.value,
+            "treasure_requirements": self.options.treasure_requirements.value
         }
 
 
